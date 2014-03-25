0b4e9b9d0236c063d5e1c6a0d0fefdd4970aa901
Macolu <github@macolu.org>
2014-03-25 22:10:46 +0100

    use owfs

--- refresh.sh
+++ refresh.sh
@@ -2,5 +2,8 @@
 PORT=2003
 SERVER=localhost
 
-echo temp.salon   `owget /uncached/28.88EE8D040000/temperature` `date +%s` | nc $SERVER $PORT
-echo temp.couloir `owget /uncached/28.FAA58E040000/temperature` `date +%s` | nc $SERVER $PORT
+#echo temp.salon   `owget /uncached/28.88EE8D040000/temperature` `date +%s` | nc $SERVER $PORT
+#echo temp.couloir `owget /uncached/28.FAA58E040000/temperature` `date +%s` | nc $SERVER $PORT
+echo temp.salon   `cat /mnt/1wire/28.88EE8D040000/temperature` `date +%s` | nc $SERVER $PORT
+echo temp.couloir `cat /mnt/1wire/28.FAA58E040000/temperature` `date +%s` | nc $SERVER $PORT
+
